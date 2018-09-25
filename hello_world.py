e582e5853f5f6a44bba6d555ca32ab6beda2e9e8
zb <2479107942@qq.com>
2018-09-25 23:58:22 +0800

    python学习

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,21 @@
-print("hello world")
-name = "abc"
-print(name.upper())
-print("\tpython")
-name1=" abc "
-name1.strip()
-print(name1)
-print(0.2+0.1)
+a=1
+print(a)
 
 
-age=23
-print("my age is "+str(age))
+#print("hello world")
+#name = "abc"
+#print(name.upper())
+#print("\tpython")
+#name1=" abc "
+#name1.strip()
+#print(name1)
+#print(0.2+0.1)
+
+
+#age=23
+#print("my age is "+str(age))
+
+#第一段python注释
+#叫列表，不叫数组
+bicycles=['z','b']
+print(bicycles[0])
